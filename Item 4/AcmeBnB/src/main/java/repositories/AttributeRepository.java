0573f5bcc4f14c0faa08ddb53da068d30de8be92
Alejandro Hall <alejandropolvillo@gmail.com>
2017-03-01 04:09:10 +0100

    Add the queries to their corresponding repositories

--- Item 4/AcmeBnB/src/main/java/repositories/AttributeRepository.java
+++ Item 4/AcmeBnB/src/main/java/repositories/AttributeRepository.java
@@ -1,12 +1,18 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Attribute;
 
 @Repository
 public interface AttributeRepository extends JpaRepository<Attribute, Integer> {
+	
+	@Query("select a from Attribute a join a.attributeValues av join av.properties p group by a order by count(a) desc")
+	Collection<Attribute> getAttributesOrderedByUse();
 
 }
