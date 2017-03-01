8cc9b965b0274fd6b31fcedaf4c3510db3653c99
Javier Santos <me@javiersantos.me>
2017-03-01 02:18:21 +0100

    Add more queries to Finder repository & service

--- Item 4/AcmeBnB/src/main/java/repositories/FinderRepository.java
+++ Item 4/AcmeBnB/src/main/java/repositories/FinderRepository.java
@@ -2,11 +2,21 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Finder;
 
 @Repository
 public interface FinderRepository extends JpaRepository<Finder, Integer> {
+
+	@Query("select avg(b.property) from Tenant t left join t.books b where t.finder.keyword like b.property.name or t.finder.keyword like b.property.description or t.finder.keyword like b.property.address")
+	Double avgResultsPerFinder();
+
+	@Query("select min(b.property) from Tenant t left join t.books b where t.finder.keyword like b.property.name or t.finder.keyword like b.property.description or t.finder.keyword like b.property.address")
+	Double minResultsPerFinder();
+
+	@Query("select max(b.property) from Tenant t left join t.books b where t.finder.keyword like b.property.name or t.finder.keyword like b.property.description or t.finder.keyword like b.property.address")
+	Double maxResultsPerFinder();
 
 }
