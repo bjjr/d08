9ddacd8051f847f5ee6633e11cbe2d4a7e5425d8
Javier Santos <me@javiersantos.me>
2017-03-01 20:22:21 +0100

    Add browse requests of Tenant

--- Item 4/AcmeBnB/src/main/java/services/BookService.java
+++ Item 4/AcmeBnB/src/main/java/services/BookService.java
@@ -1,3 +1,4 @@
+
 package services;
 
 import java.util.Collection;
@@ -17,62 +18,69 @@ import domain.Property;
 @Transactional
 @Service
 public class BookService {
-	
+
 	@Autowired
-	private BookRepository bookRepository;
-	
+	private BookRepository	bookRepository;
+
 	@Autowired
-	private TenantService tenantService;
-	
+	private TenantService	tenantService;
+
 	@Autowired
-	private StatusService statusService;
-	
-	public Book create(Property property){
+	private StatusService	statusService;
+
+
+	public Book create(Property property) {
 		Book book = new Book();
-		
+
 		Date currentDate = new Date(System.currentTimeMillis());
-		
+
 		book.setSmoker(false);
 		book.setCheckInDate(currentDate);
 		book.setCheckOutDate(DateUtil.addDays(currentDate, 1));
-		
+
 		book.setTenant(tenantService.findByPrincipal());
 		book.setProperty(property);
 		book.setStatus(statusService.create());
-		
+
 		return book;
 	}
-	
-	public Book findOne(Integer id){
+
+	public Book findOne(Integer id) {
 		return bookRepository.findOne(id);
 	}
-	
-	public Collection<Book> findAll(){
+
+	public Collection<Book> findAll() {
 		return bookRepository.findAll();
 	}
-	
-	public Book save(Book book){
+
+	public Book save(Book book) {
 		Assert.notNull(book, "BookService.save: The 'book' can not be null");
-		
+
 		Assert.isTrue(DateUtil.isOneDayAfter(book.getCheckInDate(), book.getCheckOutDate()), "BookService.save: The checkoutDate has to be one day after than checkinDate");
-		
+
 		Date currentMoment = new Date(System.currentTimeMillis());
 		Assert.isTrue(book.getCheckInDate().after(currentMoment) && book.getCheckOutDate().after(currentMoment), "BookService.save: Checkin and checkout need to be planned in the future");
-		
+
 		//Add constraint just a request pending for tenant over a single property
-		
+
 		Book result;
-		
+
 		result = bookRepository.save(book);
-		
+
 		return result;
 	}
-	
-	public void delete(Book book){
+
+	public void delete(Book book) {
 		Assert.notNull(book, "BookService.delete: The 'book' can not be null");
-		Assert.isTrue(book.getId() != 0 , "BookService.delete: The id can not be zero");
-		
+		Assert.isTrue(book.getId() != 0, "BookService.delete: The id can not be zero");
+
 		bookRepository.delete(book);
+	}
+
+	// Other business methods -------------------------------
+
+	public Collection<Book> findBooksByPrincipal() {
+		return bookRepository.findBooksByPrincipal(tenantService.findByPrincipal().getId());
 	}
 
 }
