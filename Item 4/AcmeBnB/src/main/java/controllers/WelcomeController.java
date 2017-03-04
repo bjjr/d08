19599c588a7926bcae35e4bcd565d9a9445bf8a0
Robert <rodanber@gmail.com>
2017-03-04 13:20:57 +0100

    jMenu header link modifications

--- Item 4/AcmeBnB/src/main/java/controllers/WelcomeController.java
+++ Item 4/AcmeBnB/src/main/java/controllers/WelcomeController.java
@@ -1,11 +1,11 @@
-/* WelcomeController.java
- *
+/*
+ * WelcomeController.java
+ * 
  * Copyright (C) 2017 Universidad de Sevilla
  * 
- * The use of this project is hereby constrained to the conditions of the 
- * TDG Licence, a copy of which you may download from 
+ * The use of this project is hereby constrained to the conditions of the
+ * TDG Licence, a copy of which you may download from
  * http://www.tdg-seville.info/License.html
- * 
  */
 
 package controllers;
@@ -13,32 +13,48 @@ package controllers;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import services.ActorService;
 
 @Controller
 @RequestMapping("/welcome")
 public class WelcomeController extends AbstractController {
 
 	// Constructors -----------------------------------------------------------
-	
+
 	public WelcomeController() {
 		super();
 	}
-		
+
+
+	// Services ---------------------------------------------------------------
+
+	@Autowired
+	private ActorService	actorService;
+
+
 	// Index ------------------------------------------------------------------		
 
 	@RequestMapping(value = "/index")
-	public ModelAndView index(@RequestParam(required=false, defaultValue="John Doe") String name) {
+	public ModelAndView index() {
 		ModelAndView result;
 		SimpleDateFormat formatter;
 		String moment;
-		
+		String name;
+
+		try {
+			name = actorService.findByPrincipal().getName();
+		} catch (Throwable th) {
+			name = "";
+		}
+
 		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		moment = formatter.format(new Date());
-				
+
 		result = new ModelAndView("welcome/index");
 		result.addObject("name", name);
 		result.addObject("moment", moment);
