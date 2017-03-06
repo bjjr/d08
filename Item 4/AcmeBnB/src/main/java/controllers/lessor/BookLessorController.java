d2ef0bbbd3046da023d5bafdfed46f2003720326
Robert <rodanber@gmail.com>
2017-03-07 00:04:34 +0100

    Removed unnecesary imports in BookLessorController

--- Item 4/AcmeBnB/src/main/java/controllers/lessor/BookLessorController.java
+++ Item 4/AcmeBnB/src/main/java/controllers/lessor/BookLessorController.java
@@ -13,11 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.BookService;
-import services.FeeService;
 import services.LessorService;
 import controllers.AbstractController;
 import domain.Book;
-import domain.Fee;
 import domain.Lessor;
 
 @Controller
@@ -41,35 +39,31 @@ public class BookLessorController extends AbstractController {
 		Assert.notNull(lessor);
 
 		books = bookService.findLessorBooks();
-		
-		
+
 		result = new ModelAndView("book/list");
 		result.addObject("requestUri", "/book/lessor/list.do");
 		result.addObject("books", books);
 		result.addObject("feeToPay", lessor.getAccumulatedCharges());
 
-
 		return result;
 	}
-	
+
 	@RequestMapping(value = "/accept", method = RequestMethod.GET)
-	public ModelAndView accept(@RequestParam int bookId){
+	public ModelAndView accept(@RequestParam int bookId) {
 		ModelAndView view = new ModelAndView("redirect:list.do");
-		
+
 		bookService.acceptBook(bookId);
-		
+
 		return view;
 	}
-	
+
 	@RequestMapping(value = "/deny", method = RequestMethod.GET)
-	public ModelAndView deny(@RequestParam int bookId){
+	public ModelAndView deny(@RequestParam int bookId) {
 		ModelAndView view = new ModelAndView("redirect:list.do");
-		
+
 		bookService.denyBook(bookId);
-		
+
 		return view;
 	}
-	
-	
 
 }
